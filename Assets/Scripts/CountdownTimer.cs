5047ddc26720e6dc96c29d519fd6f91e76ac3f8e
Hayate Akahori <irregularhunterxzeroaxl@gmail.com>
2026-09-07 18:13:51 +0900

    スタート前、ゲーム中のカウントダウンを追加
    スタート前、ゲーム中のカウントダウン、仮表示を追加

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI count;
+    [SerializeField] TextMeshProUGUI countFirst;
+
+    //フラグ取得用
+    FirstCountdown script;
+
+    //カウントダウン
+    public float countDown = 120.0f;
+    //表示用
+    private int _countValue;
+
+    //カウントダウン開始用フラグ
+    //private int _countDownFlag = 0;
+
+    
+    void Start() 
+    {
+        script = countFirst.GetComponent<FirstCountdown>();
+        countDown = 120.0f;
+    }
+
+    void Update()
+    {
+        if (script.start == 1)
+        {
+            //カウントダウン
+            countDown -= Time.deltaTime;
+
+            //表示用に数値をint変換(切り上げ)
+            _countValue = Mathf.CeilToInt(countDown);
+
+            //時間を表示する
+            count.text = _countValue.ToString() + "秒";
+
+            //countdownが0以下になったとき
+            if (countDown <= 0)
+            {
+                count.text = "TIME UP!!";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FirstCountdown.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstCountdown : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI countFirst;
+
+    //カウントダウン
+    public float countDownFirst = 3.0f;
+    //表示用
+    private int _countFirstValue;
+    //カウントダウン開始用フラグ
+    public int start = 0; 
+
+    void Start()
+    {
+        countDownFirst = 3.0f;
+        //タイムラグ調整用
+        countDownFirst++;
+    }
+
+    void Update()
+    {
+        if (countDownFirst >= 0.0f)
+        {
+            //カウントダウン
+            countDownFirst -= Time.deltaTime;
+
+            //表示用に数値をint変換(切り上げ)
+            _countFirstValue = Mathf.CeilToInt(countDownFirst) - 1;
+
+            //時間を表示する
+            countFirst.text = _countFirstValue.ToString();
+
+            //countdownが0以下になったとき
+            
+        }
+
+        if (countDownFirst <= 1.0f)
+        {        
+            if (countDownFirst <= 0.0f)
+            {
+                countDownFirst = 0.0f;
+                countFirst.text = "";
+                start = 1;
+            }
+            else 
+            {
+                countFirst.text = "START";
+            }
+        }
+    }
+}
