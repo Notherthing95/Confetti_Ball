ae633375d592156fb8214ce461d5565dfc8f8124
Herbert929 <flyingconsole@icloud.com>
2026-09-07 17:32:14 +0900

    現在の進捗を投げます
    Prefabフォルダを新しく作りました

---
+++ Assets/Scripts/Mouse/FingerScript.cs
@@ -0,0 +1,33 @@
+// ŠÛ‚É“–‚½‚Á‚Ä‚é‚©‚Ç‚¤‚©‚Ì”»’è
+using UnityEngine;
+
+public class FingerScript : MonoBehaviour
+{
+    public bool isTriggering;
+    public GameObject collisionObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isTriggering = true;
+        Debug.Log("triggered");
+        collisionObject = collision.gameObject;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTriggering = false;
+    }
+}
