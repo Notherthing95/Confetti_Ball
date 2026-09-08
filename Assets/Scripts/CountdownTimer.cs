afccd68c8d2cf7adea516724cb54b01455a6de2c
Hayate Akahori <irregularhunterxzeroaxl@gmail.com>
2026-09-08 14:02:46 +0900

    最初のカウントダウンとゲーム中のカウントダウンを更新
    最初のカウントダウンが正しく動作するように直したのと、ゲーム中のカウントダウンが終了した際にリザルト画面へ遷移するようになりました

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
-    //[SerializeField] TextMeshProUGUI count;
-    //[SerializeField] TextMeshProUGUI countFirst;
+    [SerializeField] TextMeshProUGUI count;
+    [SerializeField] TextMeshProUGUI countFirst;
 
     //フラグ取得用
     FirstCountdown script;
@@ -17,21 +19,36 @@ public class CountdownTimer : MonoBehaviour
     //表示用
     private int _countValue;
 
+    //画面遷移用ラグ
+    private float _rag = 0;
+
     //カウントダウン開始用フラグ
     //private int _countDownFlag = 0;
 
-    
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+
     void Start() 
     {
         //script = countFirst.GetComponent<FirstCountdown>();
+        
         script = this.GetComponent<FirstCountdown>();
         //countDown;
     }
 
     void Update()
     {
-        if (script.start == 1)
+        if (script.start == 1 || script.start == 2)
         {
+            if (script.start == 1 && Time.timeScale == 0)
+            {                
+                //Debug.Log("start:" + script.start);
+                script.start = 2;
+                Time.timeScale = 1.0f;
+            }
+            
             //カウントダウン
             countDown -= Time.deltaTime;
 
@@ -39,13 +56,31 @@ public class CountdownTimer : MonoBehaviour
             _countValue = Mathf.CeilToInt(countDown);
 
             //時間を表示する
-            //count.text = _countValue.ToString() + "秒";
+            count.text = _countValue.ToString();
 
             //countdownが0以下になったとき
             if (countDown <= 0)
             {
-                //count.text = "TIME UP!!";
+                count.text = "TIME UP!!";
+
+                Time.timeScale = 0;
+
+                //time Time.unscaledDeltaTime;
             }
+
+            if (Time.timeScale == 0 && countDown <= 0)
+            {
+                _rag += Time.unscaledDeltaTime;
+            }
+
+            if (_rag >= 1.5f)
+            {
+                ChangeScene();
+            }
+        }
+        else
+        {
+            Time.timeScale = 0;
         }
     }
 }
--- Assets/Scripts/FirstCountdown.cs
+++ Assets/Scripts/FirstCountdown.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FirstCountdown : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class FirstCountdown : MonoBehaviour
 
     void Start()
     {
+        Time.timeScale = 0;
+
         countDownFirst = 3.0f;
         //タイムラグ調整用
         countDownFirst++;
@@ -24,10 +27,17 @@ public class FirstCountdown : MonoBehaviour
 
     void Update()
     {
+        //Time.timeScale = 1;
+
+        //Debug.Log("timescale:" + Time.timeScale);
+        //Debug.Log("start:" + start);
+
+
         if (countDownFirst >= 0.0f)
         {
             //カウントダウン
-            countDownFirst -= Time.unscaledDeltaTime;
+            countDownFirst -= Time.unscaledDeltaTime % 1.0f;
+            Debug.Log(Time.unscaledDeltaTime % 1.0f);
 
             //表示用に数値をint変換(切り上げ)
             _countFirstValue = Mathf.CeilToInt(countDownFirst) - 1;
@@ -40,14 +50,14 @@ public class FirstCountdown : MonoBehaviour
         }
 
         if (countDownFirst <= 1.0f)
-        {        
+        {
             if (countDownFirst <= 0.0f)
             {
                 countDownFirst = 0.0f;
                 countFirst.text = "";
                 start = 1;
             }
-            else 
+            else
             {
                 countFirst.text = "START";
             }
