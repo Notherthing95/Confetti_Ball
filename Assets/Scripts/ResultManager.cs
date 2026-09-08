c65e9cd5586c60f34205506ddc32fef00331f06f
Herbert929 <flyingconsole@icloud.com>
2026-09-08 15:47:16 +0900

    配置
    はい

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -10,7 +10,7 @@ public class ResultManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.visible = true;
+
     }
 
     // Update is called once per frame
