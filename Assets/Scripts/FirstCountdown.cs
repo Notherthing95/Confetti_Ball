2330aa0b4b118f57c6c1192d0d61985a4771112e
Hayate Akahori <irregularhunterxzeroaxl@gmail.com>
2026-09-08 15:27:37 +0900

    両方のカウントダウンに効果音を付けました
    両方のカウントダウンに効果音を付けました(+初期化処理)

--- Assets/Scripts/FirstCountdown.cs
+++ Assets/Scripts/FirstCountdown.cs
@@ -8,16 +8,21 @@ using UnityEngine.SceneManagement;
 public class FirstCountdown : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI countFirst;
+    [SerializeField] SoundManager soundManager;
 
     //カウントダウン
     public float countDownFirst = 3.0f;
     //表示用
     private int _countFirstValue;
     //カウントダウン開始用フラグ
-    public int start = 0; 
+    public int start = 0;
+    //スタート用効果音フラグ
+    private int _start = 0;
 
     void Start()
     {
+        soundManager = this.GetComponent<SoundManager>();
+
         Time.timeScale = 0;
 
         countDownFirst = 3.0f;
@@ -37,7 +42,13 @@ public class FirstCountdown : MonoBehaviour
         {
             //カウントダウン
             countDownFirst -= Time.unscaledDeltaTime % 1.0f;
-            Debug.Log(Time.unscaledDeltaTime % 1.0f);
+            //Debug.Log(Time.unscaledDeltaTime % 1.0f);
+
+            //効果音
+            if(1.85f <= countDownFirst && countDownFirst <= 2.0f || 2.85 <= countDownFirst && countDownFirst <= 3.0f || 3.85 <= countDownFirst && countDownFirst <= 4.0f)
+            {
+                soundManager.PlayCountSE();
+            }
 
             //表示用に数値をint変換(切り上げ)
             _countFirstValue = Mathf.CeilToInt(countDownFirst) - 1;
@@ -59,6 +70,12 @@ public class FirstCountdown : MonoBehaviour
             }
             else
             {
+                if (_start == 0)
+                {
+                    soundManager.PlayStartSE();
+                    _start++;
+                }
+
                 countFirst.text = "START";
             }
         }
